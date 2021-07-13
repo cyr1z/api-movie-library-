db5e175e511f7c9f36cc3b75ee93e37ba7b48436
cyr <cyr@zolotarev.pp.ua>
2021-07-13 22:15:30 +0300

    add logout api resource

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from app.app import api
 from app.resources.countries import CountryListApi
 from app.resources.directors import DirectorListApi
 from app.resources.genres import GenreListApi
-from app.resources.login import Login
+from app.resources.login import Login, Logout
 from app.resources.movies import MovieListApi
 from app.resources.users import UserListApi
 
@@ -15,3 +15,4 @@ route(GenreListApi, "/genres", "/genres/<uuid>", strict_slashes=False)
 route(CountryListApi, "/countries", "/countries/<uuid>", strict_slashes=False)
 route(DirectorListApi, "/directors", "/directors/<uuid>", strict_slashes=False)
 route(Login, "/login", strict_slashes=False)
+route(Logout, "/logout", strict_slashes=False)
